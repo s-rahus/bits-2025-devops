b049acd83074bc7eb306c67d33ae8344cc5cbb07
Rahul U S <s.rahus@gmail.com>
2025-02-25 07:05:03 +0000

    My first commit

---
+++ sample-python.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'name': 'Flask',
+        'type': 'Web Framework',
+        'language': 'Python'
+    }
+    return jsonify(data)
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    new_data = request.json
+    return jsonify(new_data), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
